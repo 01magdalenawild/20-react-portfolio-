cb2f13b2eb99c5c52d07982d6c4ba029cec9554c
Magdalena Wild <maggieprophete2425@gmail.com>
2023-04-10 18:59:46 -0400

    adding icons

--- my-app/src/components/navbar.jsx
+++ my-app/src/components/navbar.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
-
-const navbar = () => {
-  return (
-    <div>navbar
-
-
-        
-    </div>
-  )
+import {fabars,faTimes} from 'react-icons/fa'
+const Navbar = () => {
+    return (
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0f1240] text-gray-300'>
+{/*menu*/}
+<div>
+<ul>
+    <li>Home</li>
+    <li>About</li>
+    <li>Skills</li>
+    <li>Projects</li>
+    <li>Contact</li>
+</ul>
+</div>
+{/*Hamburger*/}
+<div>
+    
+</div>
+{/*mobile menu*/}
+<div></div>
+       
+       
+       
+       {/*social icons*/}
+       <div></div>
+        </div>
+    )
 }
 
-export default navbar
+export default Navbar
